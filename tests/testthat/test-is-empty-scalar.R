c43372844bb5e2f6f10e8d213f59f0fcdf4efcfe
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-11 11:45:29 +0300

    more tests

--- tests/testthat/test-is-empty-scalar.R
+++ tests/testthat/test-is-empty-scalar.R
@@ -53,3 +53,25 @@ test_that("test.is_scalar.a_vector.returns_false", {
 test_that("test.is_scalar.empty.returns_false", {
   expect_false(is_scalar(numeric()))
 }) 
+
+test_that("test.is_scalar.a_single_elt_list.returns_true_with_length_metric", {
+  x <- list(1:5)
+  expect_true(is_scalar(x))
+  expect_false(is_scalar(x, "elements"))
+})
+
+
+test_that("test.is_of_length.length_69.returns_true_when_length_is_69", {
+  x <- 1:69
+  expect_false(is_of_length(x, 68))
+  expect_true(is_of_length(x, 69))
+  expect_false(is_of_length(x, 70))
+}) 
+
+test_that("test.has_elements.69_elts.returns_true_when_69_elts", {
+  x <- 1:69
+  expect_false(has_elements(x, 68))
+  expect_true(has_elements(x, 69))
+  expect_false(has_elements(x, 70))
+}) 
+
