854d5451b890bbbd210c7484ce56f624e3756f2c
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-19 14:07:58 +0300

    better err msg for has_no_duplicates + examples

--- R/has-dupes.R
+++ R/has-dupes.R
@@ -8,13 +8,17 @@
 #' is \code{TRUE}.  \code{assert_has_duplicates} returns nothing but 
 #' throws an error if \code{has_duplicates} is not \code{TRUE}. 
 #' \code{has_no_duplicates} is the negation of \code{has_duplicates}.
-##' @seealso \code{\link{anyDuplicated}}.
+#' @seealso \code{\link{anyDuplicated}}.
+#' @examples 
+#' x <- sample(10, 100, replace = TRUE)
+#' assert_has_duplicates(x)
+#' has_no_duplicates(x)
 #' @export
 has_duplicates <- function(x, .xname = get_name_in_parent(x))
 {
   if(!anyDuplicated(x)) 
   {
-    return(false("%s has no duplicates.", .xname))
+    return(false(gettext("%s has no duplicates."), .xname))
   }
   TRUE
 }
@@ -25,7 +29,13 @@ has_no_duplicates <- function(x, .xname = get_name_in_parent(x))
 {
   if(anyDuplicated(x)) 
   {
-    return(false("%s has duplicates.", .xname))
+    return(
+      false(
+        gettext("%s has duplicates at positions %s."), 
+        .xname, 
+        toString(which(duplicated(x)), width = 100)
+      )
+    )
   }
   TRUE
 }
